75fd8c2af344ba1058500e5dc81d4e0e5e1cfdb8
georgiy.bondarenko <gbondarenko@rbc.ru>
2022-11-14 09:20:22 +0300

    Folder

--- FileManager/ViewController.swift
+++ FileManager/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let documentDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        print(documentDir)
+        let content = try? FileManager.default.contentsOfDirectory(atPath: documentDir.path)
+        print(content)
     }
 
 
