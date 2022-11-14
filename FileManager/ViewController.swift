fe79cd5cb261ba2b10ae9d076b6c1a48bc56ffeb
georgiy.bondarenko <gbondarenko@rbc.ru>
2022-11-14 08:37:06 +0300

    Initial Commit

---
+++ FileManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileManager
+//
+//  Created by Георгий Бондаренко on 14.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
